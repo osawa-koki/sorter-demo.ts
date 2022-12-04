da4fa68bd30489a5f2a71e6592712423086e72a2
osawa-koki <github@osawa.dev>
2022-12-04 23:21:02 +0900

    select変更時イベント。

--- src/Pages/Demo.tsx
+++ src/Pages/Demo.tsx
@@ -6,12 +6,22 @@ import { sort_algorithms, SortAlgorithmType, sorting_algorithms_for_visualizatio
 
 type Props = {
   sorting_algorithm: SortAlgorithmType[];
+  selected_sorting_algorithm: SortAlgorithmType;
 };
 
 class Demo extends React.Component {
 
   state: Props = {
     sorting_algorithm: sort_algorithms,
+    selected_sorting_algorithm: sort_algorithms[0],
+  }
+
+  select_changed = (selected_option: any) => {
+    const id = selected_option.value;
+    const selected_sorting_algorithm = this.state.sorting_algorithm.find((algorithm) => algorithm.id === id);
+    if (selected_sorting_algorithm) {
+      this.setState({ selected_sorting_algorithm });
+    }
   }
 
   render() {
@@ -19,7 +29,7 @@ class Demo extends React.Component {
       <div id="Demo">
         <div id="DemoHeader">
           <div id="DemoAlgorithmSelectBox">
-            <Select options={sorting_algorithms_for_visualization} placeholder="SELECT SORTING ALGORITHM" />
+            <Select options={sorting_algorithms_for_visualization} placeholder="SELECT SORTING ALGORITHM" value={sorting_algorithms_for_visualization[0]} onChange={this.select_changed} />
           </div>
         </div>
         <div id="DemoBody"></div>
