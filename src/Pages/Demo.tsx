87388aa608f8287e710842aad8e928463dc3bd38
osawa-koki <github@osawa.dev>
2022-12-04 21:42:33 +0900

    React.Componentを継承したクラスの雛形。

---
+++ src/Pages/Demo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type Props = {
+};
+
+class Demo extends React.Component {
+
+  state: Props = {
+  }
+
+  render() {
+    return (
+      <div id="Demo">
+
+      </div>
+    );
+  }
+}
+
+export default Demo;
