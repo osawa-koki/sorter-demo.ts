18cf88328bc6a7261e2b9367d585d1efb8a69175
osawa-koki <github@osawa.dev>
2022-12-04 23:32:25 +0900

    説明の表示まで。

--- src/Pages/Demo.tsx
+++ src/Pages/Demo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Select from 'react-select';
 
 import './Demo.scss';
-import { sort_algorithms, SortAlgorithmType, sorting_algorithms_for_visualization } from '../Common/SortAlgorithm';
+import { sort_algorithms, SortAlgorithmType } from '../Common/SortAlgorithm';
 
 type Props = {
   sorting_algorithm: SortAlgorithmType[];
@@ -17,11 +17,7 @@ class Demo extends React.Component {
   }
 
   select_changed = (selected_option: any) => {
-    const id = selected_option.value;
-    const selected_sorting_algorithm = this.state.sorting_algorithm.find((algorithm) => algorithm.id === id);
-    if (selected_sorting_algorithm) {
-      this.setState({ selected_sorting_algorithm });
-    }
+    this.setState({ selected_sorting_algorithm: selected_option });
   }
 
   render() {
@@ -29,7 +25,29 @@ class Demo extends React.Component {
       <div id="Demo">
         <div id="DemoHeader">
           <div id="DemoAlgorithmSelectBox">
-            <Select options={sorting_algorithms_for_visualization} placeholder="SELECT SORTING ALGORITHM" value={sorting_algorithms_for_visualization[0]} onChange={this.select_changed} />
+            <Select options={sort_algorithms} placeholder="SELECT SORTING ALGORITHM" value={this.state.selected_sorting_algorithm} onChange={this.select_changed} />
+          </div>
+          <div id="DemoAlgorithmInfoBox">
+            <table>
+              <tbody>
+                <tr>
+                  <th>名称 (en)</th>
+                  <td>{this.state.selected_sorting_algorithm.label}</td>
+                </tr>
+                <tr>
+                  <th>名称 (ja)</th>
+                  <td>{this.state.selected_sorting_algorithm.label_ja}</td>
+                </tr>
+                <tr>
+                  <th>説明 (en)</th>
+                  <td>{this.state.selected_sorting_algorithm.description}</td>
+                </tr>
+                <tr>
+                  <th>説明 (ja)</th>
+                  <td>{this.state.selected_sorting_algorithm.description_ja}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
         <div id="DemoBody"></div>
