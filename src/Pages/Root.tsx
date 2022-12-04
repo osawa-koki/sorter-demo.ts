72c841a4a6fb561b0eda7a2379745a0dc7eba763
osawa-koki <github@osawa.dev>
2022-12-04 20:27:19 +0900

    デモ画像。

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import description_img from '../Assets/description.png';
+import demo_img from '../Assets/demo.png';
 import './Root.scss';
 
 function Root() {
@@ -8,6 +9,9 @@ function Root() {
     <div id="Root">
       <Link className='RootImg' to="/description">
         <img src={description_img} alt="description" />
+      </Link>
+      <Link className='RootImg' to="/demo">
+        <img src={demo_img} alt="demo" />
       </Link>
     </div>
   );
