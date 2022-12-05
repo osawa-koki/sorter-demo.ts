f33afbe8a51838ad04fdb6f7074e83bdb3f54a4a
osawa-koki <github@osawa.dev>
2022-12-06 03:12:04 +0900

    小さな修正。

--- src/Pages/Demo.tsx
+++ src/Pages/Demo.tsx
@@ -638,7 +638,7 @@ class Demo extends React.Component {
       while (!sorted) {
         sorted = true;
         for (let i = 1; i < array.length - 1; i += 2) {
-          if (this.state.is_resetting) return; // 中断用
+          if (this.state.is_resetting) return array; // 中断用
           if (array[i] > array[i + 1]) {
             [array[i], array[i + 1]] = [array[i + 1], array[i]];
             sorted = false;
@@ -647,7 +647,7 @@ class Demo extends React.Component {
           }
         }
         for (let i = 0; i < array.length - 1; i += 2) {
-          if (this.state.is_resetting) return; // 中断用
+          if (this.state.is_resetting) return array; // 中断用
           if (array[i] > array[i + 1]) {
             [array[i], array[i + 1]] = [array[i + 1], array[i]];
             sorted = false;
