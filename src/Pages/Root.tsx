9f74119ef3732cd3916b9b3bc7224d3253ac22bb
osawa-koki <github@osawa.dev>
2022-12-05 23:32:21 +0900

    ルートパスを設定化。

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import Settings from '../Common/Settings';
+
 import description_img from '../Assets/description.png';
 import demo_img from '../Assets/demo.png';
 import './Root.scss';
@@ -7,10 +9,10 @@ import './Root.scss';
 function Root() {
   return (
     <div id="Root">
-      <Link className='RootImg' to="/sorter-demo.ts/description">
+      <Link className='RootImg' to={`${Settings.ROOT_PATH}/description`}>
         <img src={description_img} alt="description" />
       </Link>
-      <Link className='RootImg' to="/sorter-demo.ts/demo">
+      <Link className='RootImg' to={`${Settings.ROOT_PATH}/demo`}>
         <img src={demo_img} alt="demo" />
       </Link>
     </div>
