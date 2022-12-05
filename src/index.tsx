ae1a3bcf5017c1f94695bd624f8e62d008cb3a04
osawa-koki <github@osawa.dev>
2022-12-05 23:36:04 +0900

    BrowserRouter -> HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
@@ -19,14 +19,14 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={`${Settings.ROOT_PATH}`} element={<Root />} />
         <Route path={`${Settings.ROOT_PATH}/description`} element={<Description />} />
         <Route path={`${Settings.ROOT_PATH}/demo`} element={<Demo />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
