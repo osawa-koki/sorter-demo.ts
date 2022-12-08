113646000361b15d5d6cf007896abebe6d340b64
osawa-koki <github@osawa.dev>
2022-12-09 00:51:24 +0900

    簡単な動作確認。 ルートパスの指定で問題あり。

--- next.config.js
+++ next.config.js
@@ -6,5 +6,5 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // assetPrefix: isProd ? SUB_DIRECTORY : "",
-  assetPrefix: SUB_DIRECTORY,
+  //assetPrefix: SUB_DIRECTORY,
 };
