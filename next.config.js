259590e0cf12ea4ddca328d86dcca6e28c6a5722
osawa-koki <github@osawa.dev>
2022-12-09 00:09:20 +0900

    ルートパス変えた。

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /* 公開時のサブディレクトリ */
-const SUB_DIRECTORY = "/simple-next-study.ssg.ts";
+const SUB_DIRECTORY = "/sorter-demo.ts";
 
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === "production";
