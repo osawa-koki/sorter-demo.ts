9acbb5696a63faca805c6ed52885b9f146a00870
osawa-koki <github@osawa.dev>
2022-12-09 00:41:13 +0900

    css読み込み調整。

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import './index.scss';
 
+import "./Header.scss";
+
+import './Description.scss';
+import './Demo.scss';
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
