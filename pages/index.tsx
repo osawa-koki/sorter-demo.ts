b85cbb5c92d51c20be253572bce001e9efcc06ce
osawa-koki <github@osawa.dev>
2022-12-09 14:01:41 +0900

    ページレイアウト。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import Layout from '../components/Layout'
+
 import Settings from '../Common/Settings';
 
 import description_img from '../Assets/description.png';
@@ -8,14 +10,16 @@ import demo_img from '../Assets/demo.png';
 
 function Index() {
   return (
-    <div id="Root">
-      <Link className='RootImg' href={`${Settings.ROOT_PATH}/description`}>
-        <Image src={description_img} alt="description" />
-      </Link>
-      <Link className='RootImg' href={`${Settings.ROOT_PATH}/demo`}>
-        <Image src={demo_img} alt="demo" />
-      </Link>
-    </div>
+    <Layout title="sorter-demo.ts">
+      <div id="Root">
+        <Link className='RootImg' href={`${Settings.ROOT_PATH}/description`}>
+          <Image src={description_img} alt="description" />
+        </Link>
+        <Link className='RootImg' href={`${Settings.ROOT_PATH}/demo`}>
+          <Image src={demo_img} alt="demo" />
+        </Link>
+      </div>
+    </Layout>
   );
 }
 
