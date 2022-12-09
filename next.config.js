0798bb9a296e17a3f99e1f3f5d15edb6e647ba6d
osawa-koki <github@osawa.dev>
2022-12-09 15:17:18 +0900

    画像最適化モジュール消した。

--- next.config.js
+++ next.config.js
@@ -4,12 +4,10 @@ const SUB_DIRECTORY = "/sorter-demo.ts";
 /* 本番環境と開発環境の分岐用のフラグ */
 const isProd = process.env.NODE_ENV === "production";
 
-const withExportImages = require('next-export-optimize-images')
-
-module.exports = withExportImages({
+module.exports = {
   basePath: isProd ? SUB_DIRECTORY : "",
   assetPrefix: isProd ? SUB_DIRECTORY : "",
   publicRuntimeConfig: {
     basePath: isProd ? SUB_DIRECTORY : "",
   },
-});
+};
