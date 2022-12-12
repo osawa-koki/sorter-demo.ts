42eadf1649335b7fe7c79e13da3c460af611293b
osawa-koki <github@osawa.dev>
2022-12-12 13:52:27 +0900

    設定の変更。

--- Common/Settings.ts
+++ Common/Settings.ts
@@ -1,7 +1,11 @@
+const EnvInfo = require("../next.config.js");
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV === "production";
 
 const Settings = {
   ROOT_PATH: "",
-  IMG_ROOT_PATH: "/sorter-demo.ts",
+  IMG_ROOT_PATH: isProd ? EnvInfo.assetPrefix : "",
   PAGE_DEFAULT_TITLE: "Sorter Demo",
 };
 
